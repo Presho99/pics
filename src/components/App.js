3863a41c3f02bdd1d39a2aa9d1303f16d9af5aa6
Precious <Preshomondi@gmail.com>
2023-04-29 19:25:30 +0300

    Create async request and set state

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import axios from 'axios'
+import SearchBar from './SearchBar'
+
+
+ class App extends React.Component{
+    state = {images: []}
+
+     onSearchSubmit = async (term) =>{
+      const response = await axios.get('https://api.unsplash.com/search/photos', {
+          params: { query: term},
+          headers: {
+            Authorization: 
+            'Client-ID Qr9-SCZYoYAe9PA6QWsvdG6Xuu0vrx1AX5ZT2GQxUG8'
+          }
+
+      })
+     
+    this.setState({images: response.data.results})
+    }
+   
+    render(){
+        return (
+            <div className='ui container' style={{marginTop: '10px'}}>
+                <SearchBar runMeOnSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
+            </div>
+          )
+
+    }
+  
+}
+
+export default App
