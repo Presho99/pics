3f337214f2ee20353da7f27a9b2483eebe90911d
Precious <Preshomondi@gmail.com>
2023-04-29 20:08:54 +0300

    Add keys

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const ImageList = (props) => {
-   const images = props.images.map((image) => {
-       return <img src={image.urls.regular}/>
+   const images = props.images.map(({description, id, urls}) => {
+    //    Only assign the key to the root element we are returning from the rendered list
+       return <img key={id} alt={description} src={urls.regular}/>
    })
    return(
    <div>
