f77eb587ecd7136c638fa31dd5c7fdce3be32eae
Precious <Preshomondi@gmail.com>
2023-04-30 20:22:46 +0300

    Add components

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import './ImageList.css'
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-   const images = props.images.map(({description, id, urls}) => {
+   const images = props.images.map((image) => {
     //    Only assign the key to the root element we are returning from the rendered list
-       return <img key={id} alt={description} src={urls.regular}/>
+       return <ImageCard key={image.id} image={image} />
    })
    return(
-   <div>
+   <div className='image-list'>
        {images}
    </div>
    )
   
-}
+} 
 export default ImageList
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,25 @@
 import React from "react";
 
-
 class SearchBar extends React.Component {
-    state = {term: "Hi there"}
+  state = { term: "" };
 
-    onFormSubmit = (e) => {
-        e.preventDefault()
+  onFormSubmit = (e) => {
+    e.preventDefault();
 
-        console.log(this.state.term)
-    }
+    this.props.runMeOnSubmit(this.state.term);
+  };
 
-   
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={(e) => this.onFormSubmit(e)} className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-              <label>Image Search</label>
-            <input type="text" value={this.state.term} onChange={(e)=> this.setState({term: e.target.value})}/>
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
